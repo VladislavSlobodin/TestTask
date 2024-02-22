a31ddb155b43be0bca8c78074069135c0b52f323
Vladislav Slobodin <vslobodin@bs-solutions.by>
2024-02-22 16:07:36 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class OrderService : IOrderService
+{
+    private readonly ApplicationDbContext _context;
+
+    public OrderService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<Order?> GetOrder() => _context.Orders.OrderByDescending(o => o.Quantity * o.Price).FirstOrDefaultAsync();
+    
+    public Task<List<Order>> GetOrders() => _context.Orders.Where(o => o.Quantity > 10).ToListAsync();
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class UserService : IUserService
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<User?> GetUser() => _context.Users.OrderByDescending(u => u.Orders.Count).FirstOrDefaultAsync();
+
+    public Task<List<User>> GetUsers() => _context.Users.Where(u => u.Status == Enums.UserStatus.Inactive).ToListAsync();
+}
